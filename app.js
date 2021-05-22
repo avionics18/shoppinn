af9439e9b1cb683d8e80c3fabff55ee09bb73d4d
avionics18 <avimurmu18@gmail.com>
2021-05-22 19:09:31 +0530

    pre-final changes

--- app.js
+++ app.js
@@ -83,6 +83,6 @@ app.use(userRoutes);
 app.use(paymentRoutes);
 
 // server
-app.listen(3000, () => {
-	console.log("Server running at port 3000!");
+app.listen(process.env.PORT || 3000, () => {
+	console.log("Server running successfully");
 });
